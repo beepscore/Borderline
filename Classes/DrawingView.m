24a2736acc23d9b601a22a4d333786e1ed688b74
beepscore <support@beepscore.com>
2010-05-05 00:55:30 -0700

    Finish implement addRoundedRectToPath().  Use CGFloat insteat of float.
    Passed manual test- corner radius slider clips image with rounded rect.

--- Classes/DrawingView.m
+++ Classes/DrawingView.m
@@ -30,15 +30,38 @@
 }
 
 // Ref Gelphman Ch 6 pg 114-115
-void addRoundedRectToPath(CGContextRef context, CGRect rect, float ovalWidth, float ovalHeight) {
-    // float fw, fh;
+void addRoundedRectToPath(CGContextRef context, CGRect rect, CGFloat ovalWidth, CGFloat ovalHeight) {
+    
+    CGFloat fw, fh;
     // if either ovalWidth or ovalHeight is 0, add a regular rectangle
     if ((0 == ovalWidth) || (0 == ovalHeight)) {
         CGContextAddRect(context, rect);
     } else {
-        CGContextAddRect(context, rect);
+        CGContextSaveGState(context);
+        // Translate to lower-left corner of rectangle
+        CGContextTranslateCTM(context, CGRectGetMinX(rect), CGRectGetMinY(rect));
+        // Scale by the oval width and height so that
+        // each rounded corner is 0.5 units in radius
+        CGContextScaleCTM(context, ovalWidth, ovalHeight);
+        // Unscale the rectangle width by the amount of the x scaling
+        fw = CGRectGetWidth(rect) / ovalWidth;
+        // Unscale the rectangle height by the amount of the y scaling
+        fh = CGRectGetHeight(rect) / ovalHeight;
+        // Start at the right edge of the rectangle, at the midpoint in y
+        CGContextMoveToPoint(context, fw, fh/2);
+        // Segment 1
+        CGContextAddArcToPoint(context, fw, fh, fw/2, fh, 0.5);
+        // Segment 2
+        CGContextAddArcToPoint(context, 0, fh, 0, fh/2, 0.5);
+        // Segment 3
+        CGContextAddArcToPoint(context, 0, 0, fw/2, 0, 0.5);
+        // Segment 4
+        CGContextAddArcToPoint(context, fw, 0, fw, fh/2, 0.5);
+        // Closing the path adds the last segment
+        CGContextClosePath(context);
+        
+        CGContextRestoreGState(context);
     }
-
 }
 
 
@@ -52,8 +75,10 @@ void addRoundedRectToPath(CGContextRef context, CGRect rect, float ovalWidth, fl
     
     CGContextSaveGState(graphicsContext);
 
-    CGRect clipRect = CGRectMake(30.0, 30.0, 150.0, 200.0);
-    addRoundedRectToPath(graphicsContext, clipRect, 5.0, 10.0);
+    CGRect clipRect = CGRectMake(30.0, 30.0, 220.0, 220.0);
+    CGFloat ovalWidth = [self cornerRadius];
+    CGFloat ovalHeight = ovalWidth;
+    addRoundedRectToPath(graphicsContext, clipRect, ovalWidth, ovalHeight);
     CGContextClip(graphicsContext);
     
     
